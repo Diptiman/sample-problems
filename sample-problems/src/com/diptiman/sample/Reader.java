f2698e9d1b4a3ade6d59a879e08de29bcd08f118
diptiman.adak <diptiman.adak@appdynamics.com>
2020-07-20 10:04:16 +0530

    Reader

---
+++ sample-problems/src/com/diptiman/sample/Reader.java
@@ -0,0 +1,39 @@
+package com.diptiman.sample;
+
+/**
+ * The read4 API is defined in the parent class Reader4.
+ *     int read4(char[] buf4);
+ */
+
+public class Reader {
+
+    static int head = 0;
+    static int start = head;
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return    The number of actual characters read
+     */
+
+    public static int read(char[] buf, int n) {
+        int numberOfChars = 0;
+            if(head < buf.length) {
+                start = head;
+                if(head + n < buf.length) {
+                    head = head + n;
+                } else {
+                    head = buf.length -1;
+                }
+                numberOfChars = head - start;
+            }
+        return numberOfChars;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(read(new char[] {'a', 'b', 'c', 'd', 'e'}, 4));
+        System.out.println(read(new char[] {'a', 'b', 'c', 'd', 'e'}, 4));
+    }
+
+
+}
